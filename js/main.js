33cbbfe5e2869617b4eda11ff20970e7257d3ea8
Dara Olayebi <dara.olayebi@yahoo.com>
2018-12-18 16:40:18 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -21,43 +21,39 @@ function listView() {
 }
 
 function gridView() {
+    
     document.querySelector('.cards').classList.remove('list-view')
     document.querySelector('.categories-container').classList.remove('sm')
     document.querySelector('[data-layout="grid"] svg path').style.fill = '#D0021B'
     document.querySelector('[data-layout="list"] svg path').style.fill = '#C4CDD5'
 }
 
-function showSearchBar() {
-    var menuHide = document.querySelectorAll('.jsHideToSearch')
-    document.querySelector('.nav-search').style.display = 'inline-block'
-    document.querySelector('.nav-search').style.opacity = '1'
-    document.querySelector('.close-search').style.display = 'inline-block'
-    for (var i = 0; i < menuHide.length; i++) {
-        menuHide[i].style.display = 'none'
-    }
-}
-
-function closeSearch() {
-    var menuHide = document.querySelectorAll('.jsHideToSearch')
-    document.querySelector('.nav-search').style.display = 'none'
-    document.querySelector('.nav-search').style.opacity = '0'
-    document.querySelector('.close-search').style.display = 'none'
-    for (var i = 0; i < menuHide.length; i++) {
-        menuHide[i].style.display = 'inline-block'
-    }
-}
-// document.querySelector('.close-search').addEventListener('click', function () {
-//   document.querySelector('.nav-search').style.display = 'none'
-//   document.querySelector('.nav-search').style.opacity = '0'
-//   document.querySelector('.close-search').style.display = 'none'
-//   for (var i = 0; i < menuHide.length; i++) {
-//     menuHide[i].style.display = 'inline-block'
-//   }
-// })
-
 $(document).ready(function () {
     $('.other-amount').on('focus', function () {
         console.log('h')
         $(this).next("span").css("display", "inline").fadeOut(1000);
     })
+
+    $('.tabs li').click(function () {
+        //var tab_id = $(this).attr('data-tab');
+        var tab_id = $(this).attr('href');
+        $('.tabs li, .tab-link, .tab-content').removeClass('current');
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    })
+
+    $('.categories a').on('click', function () {
+        var target = $(this).attr('href');
+        $(this).addClass('active').siblings().removeClass('active')
+        console.log(target)
+        $(target).show().siblings().hide();
+    });
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 900, 'linear');
+    });
+
 });
